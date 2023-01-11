28e6ccd0eba335588ca20924cd39a6510ee1b313
mehmet_gur <mehmet.gur@getir.com>
2023-01-12 02:29:45 +0300

    noteDao interface and crud functions added

---
+++ app/src/main/java/com/example/noteappcompose/note/data/source/NoteDao.kt
@@ -0,0 +1,25 @@
+package com.example.noteappcompose.note.data.source
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.noteappcompose.note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+
+    @Query("SELECT * FROM note")
+    fun getNote(): Flow<List<Note>>
+
+    @Query("SELECT * FROM note WHERE id = :id")
+    suspend fun getNote(id: Int): Note?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+}
