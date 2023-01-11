e310d04c4341d46058a007c5c14d40c21c4340a0
mehmet_gur <mehmet.gur@getir.com>
2023-01-12 02:33:08 +0300

    noteDatabase added

---
+++ app/src/main/java/com/example/noteappcompose/note/data/source/NoteDatabase.kt
@@ -0,0 +1,14 @@
+package com.example.noteappcompose.note.data.source
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.noteappcompose.note.domain.model.Note
+
+@Database(
+    entities = [Note::class],
+    version = 1
+)
+abstract class NoteDatabase : RoomDatabase() {
+
+    abstract val noteDao: NoteDao
+}
