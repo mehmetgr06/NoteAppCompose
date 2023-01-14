faefaa1a76bb96ca2515d328974c92af688f5f96
mehmet_gur <mehmet.gur@getir.com>
2023-01-15 00:48:21 +0300

    custom text field added

---
+++ app/src/main/java/com/example/noteappcompose/note/presentation/add_note/components/TransparentTextField.kt
@@ -0,0 +1,47 @@
+package com.example.noteappcompose.note.presentation.add_note.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+
+@Composable
+fun TransparentTextField(
+    text: String,
+    hint: String,
+    modifier: Modifier = Modifier,
+    isHintVisible: Boolean = true,
+    onValueChange: (String) -> Unit,
+    textStyle: TextStyle = TextStyle(),
+    isSingleLine: Boolean = false,
+    onFocusChange: (FocusState) -> Unit
+) {
+    Box(
+        modifier = modifier
+    ) {
+        BasicTextField(
+            value = text,
+            onValueChange = onValueChange,
+            singleLine = isSingleLine,
+            textStyle = textStyle,
+            modifier = Modifier
+                .fillMaxWidth()
+                .onFocusChanged {
+                    onFocusChange(it)
+                }
+        )
+        if (isHintVisible) {
+            Text(
+                text = hint,
+                style = textStyle,
+                color = Color.DarkGray
+            )
+        }
+    }
+}
